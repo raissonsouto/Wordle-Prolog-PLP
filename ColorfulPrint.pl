6af2f538aa2f497d2ceb7fd87219982b3b3869b9
raissonsouto <raisson.souto@ccc.ufcg.edu.br>
2022-03-29 20:51:35 -0300

    Adicionando funções inacabadas no Jogo e no ColorfulPrint.

--- ColorfulPrint.pl
+++ ColorfulPrint.pl
@@ -1,1 +1,12 @@
-colorfulPrint(tentativas, metadados):- write("teste").
+concatenate(StringList, StringResult) :-
+    maplist(atom_chars, StringList, Lists),
+    append(Lists, List),
+    atom_chars(StringResult, List).
+
+colorString(X, "red", Saida) :- concatenate(["\u001b[31m", X, "\u001b[0m"], Saida).
+colorString(X, "green", Saida) :- concatenate( ["\u001b[32m", X, "\u001b[0m"], Saida).
+colorString(X, "yellow", Saida) :- concatenate( ["\u001b[33m", X, "\u001b[0m"], Saida).
+
+textColorizer():-
+
+colorfulPrint():-
